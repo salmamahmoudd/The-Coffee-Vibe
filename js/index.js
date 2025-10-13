22a7acd267d4bb112d41f8a1d50f94d3585a9540
Salma Mahmoud <salmamahmoudd.com>
2025-10-14 00:10:15 +0300

    تعديلات جديدة

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 var menuToggle = document.querySelector('.menu-toggle'); 
 var navLinks = document.querySelector('.nav-links'); 
+var nav = document.querySelector('nav');
 
-menuToggle.addEventListener('click', function () {
-  navLinks.classList.toggle('open'); 
-});
+if(menuToggle){
+  menuToggle.addEventListener('click', function (e){
+    navLinks.classList.toggle('open');
+  })
+  document.addEventListener('click' , function(e){
+    if(!nav.contains(e.target)){
+      navLinks.classList.remove('open');
+    }
+  })
+}
